2cdc97b852c23deb68197b0bd11efdf2f350ea58
Gustavo Atala <57265955+globalisdev@users.noreply.github.com>
2022-08-30 18:08:00 -0300

    Update use_cases.py

--- exercises/exercise_1/use_cases.py
+++ exercises/exercise_1/use_cases.py
@@ -6,4 +6,4 @@ def get_textbook_subject(textbook_id) -> str:
     :param textbook_id: Specifies which textbook to lookup
     :return: Get the subject of a textbook
     """    
-    return Textbook.objects.select_related('subject').get(id=textbook_id).subject.name    
+    return Textbook.objects.get(id=textbook_id).subject.name    
